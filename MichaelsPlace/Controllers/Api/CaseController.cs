dbd5723281a30371996821aa461c1ed77605cc24
Andrew Greenman <agreenman@HAGERTY.COM>
2016-10-28 16:03:03 -0400

    Created API endpoint to create a full Case based off the Situation.

--- MichaelsPlace/Controllers/Api/CaseController.cs
+++ MichaelsPlace/Controllers/Api/CaseController.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
+using MediatR;
+using MichaelsPlace.CommandHandlers;
 using MichaelsPlace.Infrastructure;
 using MichaelsPlace.Models;
 using MichaelsPlace.Models.Api;
 using MichaelsPlace.Models.Persistence;
 using MichaelsPlace.Queries;
+using Microsoft.AspNet.Identity;
 
 namespace MichaelsPlace.Controllers.Api
 {
@@ -15,10 +20,12 @@ namespace MichaelsPlace.Controllers.Api
     public class CaseController : SpaApiControllerBase
     {
         private readonly IQueryFactory _queryFactory;
+        private readonly IMediator _mediator;
 
-        public CaseController(IQueryFactory queryFactory)
+        public CaseController(IQueryFactory queryFactory, IMediator mediator)
         {
             _queryFactory = queryFactory;
+            _mediator = mediator;
         }
 
         /// <summary>
@@ -26,11 +33,25 @@ namespace MichaelsPlace.Controllers.Api
         /// </summary>
         /// <param name="personId"></param>
         /// <returns></returns>
-        [HttpGet, Route("cases/{personId}")]
+        [HttpGet, Route("cases/{personId}")] // todo : Remove person id and pass it in through the backend
         public List<CaseModel> CasesByPerson(string personId) =>
             _queryFactory.Create<CasesByPersonQuery>()
                          .Execute<Case>(personId)
                          .ProjectTo<CaseModel>()
                          .ToList();
+
+        [HttpPost, Route("create/{situation:situation}")]
+        public async Task<string> CreateCaseBySituation(SituationModel situation)
+        {
+            var request = new CreateCaseCommand.Request()
+            {
+                Situation = situation,
+                UserId = User.Identity.GetUserId()
+            };
+
+            var result = await _mediator.SendAsync(request);
+
+            return result.Result as string;
+        }
     }
 }
