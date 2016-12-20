3fbb342c726ee1ba204c828c8ecf635c43aff5b6
Andrew Greenman <agreenman@HAGERTY.COM>
2016-12-20 12:11:16 -0500

    Added validation to create-organization-case.

--- MichaelsPlace/app/src/routes/organization/create-organization-case.js
+++ MichaelsPlace/app/src/routes/organization/create-organization-case.js
@@ -1,15 +1,20 @@
-﻿import {inject} from 'aurelia-framework';
+﻿import {inject, NewInstance} from 'aurelia-framework';
 import {Api} from 'services/api';
 import {log} from 'services/log';
 import {Router} from 'aurelia-router';
+import {ValidationController, ValidationRules} from 'aurelia-validation';
 
-@inject(Api, Router)
+@inject(Api, Router, NewInstance.of(ValidationController))
 export class CreateOrganizationCase {
 
     constructor(api, router, validationController) {
         this.api = api;
         this.router = router;
         this.validationController = validationController;
+
+        ValidationRules
+            .ensure('title').required()
+            .on(CreateOrganizationCase);
     }
 
     activate(params) {
@@ -18,15 +23,19 @@ export class CreateOrganizationCase {
     }
 
     createOrganizationCase() {
-        var page = this;
+        this.validationController.validate().then(errors => {
+            if (errors.length === 0) {
+                var page = this;
         
-        this.api.cases.createOrganizationCase(this.situation, this.title, this.organizationId)
-            .then(function(caseId) {
-                if (!caseId) {
-                    log.error('Error: Unable to create organization case.');
-                }
+                this.api.cases.createOrganizationCase(this.situation, this.title, this.organizationId)
+                    .then(function(caseId) {
+                        if (!caseId) {
+                            log.error('Error: Unable to create organization case.');
+                        }
 
-                page.router.navigateToRoute('organization-dashboard', { organizationId: page.organizationId });
-            });
+                        page.router.navigateToRoute('organization-dashboard', { organizationId: page.organizationId });
+                    });
+            }
+        });
     }
 }
