df9816fcfbe1776816fab2b1f3ff7893a159e51d
Andrew Greenman <agreenman@HAGERTY.COM>
2016-12-20 12:02:41 -0500

    Added validation to the email form.

--- MichaelsPlace/app/src/routes/auth-case/routes/email.js
+++ MichaelsPlace/app/src/routes/auth-case/routes/email.js
@@ -1,25 +1,36 @@
-﻿import {inject} from 'aurelia-framework';
+﻿import {inject, NewInstance} from 'aurelia-framework';
 import {Api} from 'services/api';
 import {log} from 'services/log';
 import {Router} from 'aurelia-router';
+import {ValidationController, ValidationRules} from 'aurelia-validation';
 
-@inject(Api, Router)
+@inject(Api, Router, NewInstance.of(ValidationController))
 export class Email {
-    constructor(api, router) {
+    constructor(api, router, validationController) {
         this.api = api;
         this.router = router;
+        this.validationController = validationController;
+
+        ValidationRules
+            .ensure('subject').required()
+            .ensure('message').required()
+            .on(Email);
     }
 
     /**
      * Sends the email to Michael's Place staff and displays the 'program' route.
      */
     sendEmail() {
-        var email = this;
+        this.validationController.validate().then(errors => {
+            if (errors.length === 0) {
+                var email = this;
 
-        this.api.email.sendToStaff(this.subject, this.message)
-            .then(function() {
-                log.debug('Email sent succesfully. Rerouting to program page.');
-                email.router.navigateToRoute('program');
-            });
+                this.api.email.sendToStaff(this.subject, this.message)
+                    .then(function() {
+                        log.debug('Email sent succesfully. Rerouting to program page.');
+                        email.router.navigateToRoute('program');
+                    });
+            }
+        });
     }
 }
