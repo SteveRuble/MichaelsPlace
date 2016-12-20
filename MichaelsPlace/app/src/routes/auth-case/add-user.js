431872a5852a6e299264a0557889a24e2d9ad88d
Andrew Greenman <agreenman@HAGERTY.COM>
2016-12-20 11:50:51 -0500

    Added validation to add-user.

--- MichaelsPlace/app/src/routes/auth-case/add-user.js
+++ MichaelsPlace/app/src/routes/auth-case/add-user.js
@@ -1,14 +1,20 @@
-﻿import {inject} from 'aurelia-framework';
+﻿import {inject, NewInstance} from 'aurelia-framework';
 import {Api} from 'services/api';
 import {log} from 'services/log';
+import {ValidationController, ValidationRules} from 'aurelia-validation';
 
-@inject(Api)
+@inject(Api, NewInstance.of(ValidationController))
 export class AddUser {
 
-    constructor(api) {
+    constructor(api, validationController) {
         this.api = api;
         this.newUser = '';
         this.additionalUsers = [];
+        this.validationController = validationController;
+
+        ValidationRules
+            .ensure('newUser').email().required()
+            .on(AddUser);
     }
 
     activate(params) {
@@ -16,21 +22,25 @@ export class AddUser {
     }
 
     sendInvitations() {
-        var addresses = [];
-        addresses.push(this.newUser);
-        for (var i = 0; i < this.additionalUsers.length; i++) {
-            addresses.push(this.additionalUsers[i]);
-        }
-
-        this.api.email.sendCaseInvitations(addresses, this.caseId)
-            .then(function(result) {
-                if (result) {
-                    alert("Invitations sent.");
-                } else {
-                    log.debug('Error sending invitations.');
-                    alert("Error sending invitations.");
+        this.validationController.validate().then(errors => {
+            if (errors.length === 0) {
+                var addresses = [];
+                addresses.push(this.newUser);
+                for (var i = 0; i < this.additionalUsers.length; i++) {
+                    addresses.push(this.additionalUsers[i]);
                 }
-            });
+
+                this.api.email.sendCaseInvitations(addresses, this.caseId)
+                    .then(function(result) {
+                        if (result) {
+                            alert("Invitations sent.");
+                        } else {
+                            log.debug('Error sending invitations.');
+                            alert("Error sending invitations.");
+                        }
+                    });
+            }
+        });
     }
 
     addUser() {
